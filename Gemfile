0bff85f8f965382fd1d0fa5a3a66354210dcdcf0
Nick Hengeveld <nickh@verticalresponse.com>
2010-09-24 21:20:05 -0700

    Fix deprecation warnings

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.0'
 gem 'sqlite3-ruby', :require => 'sqlite3'
-gem 'authlogic'
+gem 'authlogic', :git => 'git://github.com/odorcicd/authlogic.git', :branch => 'rails3'
 gem 'haml'
 
 group :development do
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -27,7 +27,12 @@ describe UsersController do
       end.should change(User, :count).by(1)
     end
 
-    it 'creates a user session?'
+    it 'creates a user session for the new user' do
+      post :create, :user => @user_attrs
+      user_session = UserSession.find
+      user_session.should_not be_nil
+      user_session.user.should == assigns[:user]
+    end
 
     it 'redirects to the show page' do
       post :create, :user => @user_attrs
